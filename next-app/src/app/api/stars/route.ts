a3d79dc5c1367f32b0277d7e038b6dbda5727408
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-01-28 23:35:27 +0530

    changed response type

--- next-app/src/app/api/stars/route.ts
+++ next-app/src/app/api/stars/route.ts
@@ -1,8 +1,25 @@
 import { NextResponse } from "next/server";
 
+type Body = {
+  action: "created" | "deleted";
+  starred_at: Date;
+  repository: {
+    name: "draw";
+    full_name: "macintushar/draw";
+    url: "https://api.github.com/repos/macintushar/draw";
+    stargazers_count: 34;
+    watchers_count: 34;
+  };
+  sender: {
+    login: "macintushar";
+  };
+};
+
 export async function POST(request: Request) {
-  const body = (await request.json()) as Record<string, string>;
-  console.log(body);
+  const body = (await request.json()) as Body;
+  console.log(
+    `${body.sender.login} starred the *${body.repository.full_name}* repository.`,
+  );
 
   return NextResponse.json({ body });
 }
